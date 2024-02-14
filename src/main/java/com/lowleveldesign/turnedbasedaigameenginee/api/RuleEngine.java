70d7501e161060b5b96b8efdaf75d6239e273808
Madhan Chiluka <madhan.chiluka@gmail.com>
2024-02-14 22:51:17 +0530

    Changes for open-closed principle

--- src/main/java/com/lowleveldesign/turnedbasedaigameenginee/api/RuleEngine.java
+++ src/main/java/com/lowleveldesign/turnedbasedaigameenginee/api/RuleEngine.java
@@ -1,52 +1,106 @@
 package com.lowleveldesign.turnedbasedaigameenginee.api;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
 import com.lowleveldesign.turnedbasedaigameenginee.boards.TicTacToeBoard;
 import com.lowleveldesign.turnedbasedaigameenginee.game.Board;
+import com.lowleveldesign.turnedbasedaigameenginee.game.Cell;
+import com.lowleveldesign.turnedbasedaigameenginee.game.GameInfo;
+import com.lowleveldesign.turnedbasedaigameenginee.game.GameInfoBuilder;
 import com.lowleveldesign.turnedbasedaigameenginee.game.GameState;
+import com.lowleveldesign.turnedbasedaigameenginee.game.Move;
+import com.lowleveldesign.turnedbasedaigameenginee.game.Player;
 
 public class RuleEngine {
-    public GameState getState(Board board) {
-        if (board instanceof TicTacToeBoard) {
-            TicTacToeBoard board1 = (TicTacToeBoard) board;
 
-            GameState rowWin = outerTraversal((i, j) -> board1.getSymbol(i, j));
-            if (rowWin.isOver())
-                return rowWin;
+    Map<String, List<Rule<TicTacToeBoard>>> ruleMap = new HashMap<>();
 
-            GameState colWin = outerTraversal((i, j) -> board1.getSymbol(j, i));
-            if (colWin.isOver())
-                return colWin;
-
-            GameState diagWin = traverse(i -> board1.getSymbol(i, i));
-            if (diagWin.isOver())
-                return diagWin;
-
-            GameState revDiagWin = traverse(i -> board1.getSymbol(i, 2 - i));
-            if (revDiagWin.isOver()) {
-                return revDiagWin;
-            }
-
+    public RuleEngine() {
+        ruleMap.put(TicTacToeBoard.class.getName(), new ArrayList<>());
+        ruleMap.get(TicTacToeBoard.class.getName())
+                .add(new Rule<>(board -> outerTraversal((row, col) -> board.getSymbol(row, col))));
+        ruleMap.get(TicTacToeBoard.class.getName())
+                .add(new Rule<>(board -> outerTraversal((row, col) -> board.getSymbol(col, row))));
+        ruleMap.get(TicTacToeBoard.class.getName()).add(new Rule<>(board -> traverse(i -> board.getSymbol(i, i))));
+        ruleMap.get(TicTacToeBoard.class.getName()).add(new Rule<>(board -> traverse(i -> board.getSymbol(i, 2 - i))));
+        ruleMap.get(TicTacToeBoard.class.getName()).add(new Rule<>(board -> {
             int countOfFilledCells = 0;
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
-                    if (board1.getSymbol(i, j) != null) {
+                    if (board.getSymbol(i, j) != null) {
                         countOfFilledCells++;
                     }
                 }
             }
-
             if (countOfFilledCells == 9) {
                 return new GameState(true, "-");
-            } else {
-                return new GameState(false, "-");
             }
+            return new GameState(false, "-");
+        }));
+    }
+
+    public GameInfo getInfo(Board board) {
+        if (board instanceof TicTacToeBoard) {
+            GameState gameState = getState(board);
+            final String[] players = new String[] { "X", "O" };
+            for (int index = 0; index < 2; index++) {
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        Board b = board.copy();
+                        Player player = new Player(players[index]);
+                        b.move(new Move(new Cell(i, j), player));
+                        boolean canStillWin = true;
+                        for (int k = 0; k < 3; k++) {
+                            for (int l = 0; l < 3; l++) {
+                                Board b1 = b.copy();
+                                b1.move(new Move(new Cell(k, l), player.flip()));
+                                if (getState(b1).getWinner().equals(player.flip().symbol())) {
+                                    canStillWin = true;
+                                    break;
+                                }
+                            }
+                            if (canStillWin) {
+                                break;
+                            }
+                        }
+                        if (canStillWin) {
+                            return new GameInfoBuilder()
+                                    .isOver(gameState.isOver())
+                                    .winner(gameState.getWinner())
+                                    .hasFork(true)
+                                    .player(player.flip())
+                                    .build();
+                        }
+                    }
+                }
+            }
+            return new GameInfoBuilder().isOver(gameState.isOver()).winner(gameState.getWinner()).build();
         } else {
             throw new IllegalArgumentException();
         }
     }
+
+    public GameState getState(Board board) {
+        if (board instanceof TicTacToeBoard) {
+            TicTacToeBoard board1 = (TicTacToeBoard) board;
+            for (Rule<TicTacToeBoard> rule : ruleMap.get(TicTacToeBoard.class.getName())) {
+                GameState gameState = rule.condition.apply(board1);
+                if (gameState.isOver()) {
+                    return gameState;
+                }
+            }
+            return new GameState(false, "-");
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private Function<TicTacToeBoard, GameState> outerTraversal = (board -> outerTraversal(board::getSymbol));
 
     private GameState outerTraversal(BiFunction<Integer, Integer, String> next) {
         GameState result = new GameState(false, "-");
@@ -75,4 +129,13 @@ public class RuleEngine {
         }
         return result;
     }
+}
+
+class Rule<T extends Board> {
+    Function<T, GameState> condition;
+
+    public Rule(Function<T, GameState> condition) {
+        this.condition = condition;
+    }
+
 }
---
+++ src/main/java/com/lowleveldesign/turnedbasedaigameenginee/game/GameInfo.java
@@ -0,0 +1,21 @@
+package com.lowleveldesign.turnedbasedaigameenginee.game;
+
+import lombok.Data;
+
+@Data
+public class GameInfo {
+    private boolean isOver;
+    private String winner;
+    private boolean hasFork;
+    private Player player;
+    private int numberOfMoves;
+
+    public GameInfo(boolean isOver, String winner, boolean hasFork, Player player, int numberOfMoves) {
+        this.isOver = isOver;
+        this.winner = winner;
+        this.hasFork = hasFork;
+        this.player = player;
+        this.numberOfMoves = numberOfMoves;
+    }
+
+}
